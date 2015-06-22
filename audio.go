182b9a9912d4099f0e1c0d6d903b28468688e4fc
Chris Reeves <chris@thisissoon.com>
2015-06-22 14:08:31 +0100

    Lowering the buffer size

--- audio.go
+++ audio.go
@@ -19,7 +19,7 @@ var (
 
 	// audioOutputBufferSize is the maximum number of bytes to buffer before
 	// passing it to PortAudio.
-	audioOutputBufferSize = 8192
+	audioOutputBufferSize = 1024
 )
 
 // audio wraps the delivered Spotify data into a single struct.
