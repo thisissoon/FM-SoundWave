25fbbcff65d2910ff122f45febba8bf09c48bf32
Chris Reeves <chris@thisissoon.com>
2015-09-04 09:57:03 +0100

    Player package constants

---
+++ player/const.go
@@ -0,0 +1,9 @@
+// Player Package Constants
+
+package player
+
+const (
+	APPLICATION_NAME  string = "SFM_"
+	CACHE_LOCATION    string = "/tmp/soundwave"
+	SETTINGS_LOCATION string = "/tmp/soundwave"
+)
