604a51ae5230daad739a9901c72350a530893dab
Chris Reeves <chris@thisissoon.com>
2015-09-04 09:41:08 +0100

    Use viper for config rather than cli flags

--- cmd/soundwave/soundwave.go
+++ cmd/soundwave/soundwave.go
@@ -5,14 +5,14 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/thisissoon/FM-SoundWave"
+	"github.com/spf13/viper"
+	"github.com/thisissoon/FM-SoundWave/events"
 	"github.com/thisissoon/FM-SoundWave/perceptor"
-	redis "gopkg.in/redis.v3"
 )
 
 var (
@@ -34,37 +34,47 @@ var SoundWaveCmd = &cobra.Command{
 	Short: "Play Spotify Music for SOON_ FM",
 	Long:  soundWaveCmdLongDesc,
 	Run: func(cmd *cobra.Command, args []string) {
+		// Set log level
+		log.SetLevel(log.DebugLevel)
+
+		// Make event channels
+		channels := events.NewChannels()
+
+		// Make Event Handler
+		handler := events.NewHandler(channels)
+		go handler.Run()
+
 		// Create Perceptor
-		p := perceptor.New(perceptorAddr, secret)
+		p := perceptor.New(perceptorAddr, secret, handler.ReceiveChannel())
 		go p.WSConnection()
 
-		// Create a new Player
-		player, err := soundwave.NewPlayer(&spotify_user, &spotify_pass, &spotify_key)
-		if err != nil {
-			log.Fatalln(err) // Exit on failure to create player
-		}
+		// // Create a new Player
+		// player, err := soundwave.NewPlayer(&spotify_user, &spotify_pass, &spotify_key)
+		// if err != nil {
+		// 	log.Fatalln(err) // Exit on failure to create player
+		// }
 
-		defer player.Session.Close() // Close Session
-		defer player.Audio.Close()   // Close Audio Writer
+		// defer player.Session.Close() // Close Session
+		// defer player.Audio.Close()   // Close Audio Writer
 
-		// Create a redis client
-		redis_client := redis.NewClient(&redis.Options{
-			Network: "tcp",
-			Addr:    redis_address,
-		})
+		// // Create a redis client
+		// redis_client := redis.NewClient(&redis.Options{
+		// 	Network: "tcp",
+		// 	Addr:    redis_address,
+		// })
 
-		// Create playlist
-		playlist := soundwave.NewPlaylist(redis_queue, redis_channel, redis_client, player)
-		// Watch playlist
-		go playlist.Watch()
+		// // Create playlist
+		// playlist := soundwave.NewPlaylist(redis_queue, redis_channel, redis_client, player)
+		// // Watch playlist
+		// go playlist.Watch()
 
-		// Publish track duration
-		go playlist.CurrentTrackDurationPublisher()
+		// // Publish track duration
+		// go playlist.CurrentTrackDurationPublisher()
 
-		// Create Event Reactor
-		reactor := soundwave.NewReactor(redis_channel, redis_client, player)
-		// Spin off reactor consumer gorountiune
-		go reactor.Consume()
+		// // Create Event Reactor
+		// reactor := soundwave.NewReactor(redis_channel, redis_client, player)
+		// // Spin off reactor consumer gorountiune
+		// go reactor.Consume()
 
 		// Channel to listen for OS Signals
 		signals := make(chan os.Signal, 1)
@@ -79,17 +89,42 @@ var SoundWaveCmd = &cobra.Command{
 }
 
 func init() {
-	// Spotify Flags
-	SoundWaveCmd.Flags().StringVarP(&spotify_user, "user", "u", "", "Spotify User")
-	SoundWaveCmd.Flags().StringVarP(&spotify_pass, "pass", "p", "", "Spotify Password")
-	SoundWaveCmd.Flags().StringVarP(&spotify_key, "key", "k", "", "Spotify Key Path")
-	// Redis Flags
-	SoundWaveCmd.Flags().StringVarP(&redis_address, "redis", "r", "127.0.0.1:6379", "Redis Server Address")
-	SoundWaveCmd.Flags().StringVarP(&redis_queue, "queue", "q", "", "Redis Queue Name")
-	SoundWaveCmd.Flags().StringVarP(&redis_channel, "channel", "c", "", "Redis Channel Name")
-	// Perceptor Flags
-	SoundWaveCmd.Flags().StringVarP(&perceptorAddr, "perceptor", "t", "perceptor.thisissoon.fm", "Perceptor Address")
-	SoundWaveCmd.Flags().StringVarP(&secret, "secret", "s", "CHANGE_ME", "Secret Key")
+	// Load config from File
+	log.SetLevel(log.WarnLevel)
+
+	// Defaults
+	viper.SetDefault("perceptor_address", "localhost:9000")
+	viper.SetDefault("secret", "fooo")
+	viper.SetDefault("log_level", "warn")
+	viper.SetDefault("spotify", map[string]string{
+		"user": "CHANGE_ME",
+		"pass": "CHANGE_ME",
+		"key":  "CHANGE_ME",
+	})
+
+	// From file
+	viper.SetConfigName("config")           // name of config file (without extension)
+	viper.AddConfigPath("/etc/soundwave/")  // path to look for the config file in
+	viper.AddConfigPath("$HOME/.soundwave") // call multiple times to add many search paths
+	viper.AddConfigPath("$PWD/.soundwave")  // call multiple times to add many search paths
+	err := viper.ReadInConfig()             // Find and read the config file
+	if err != nil {                         // Handle errors reading the config file
+		log.Warnf("No config file found or is not properly formatted: %s", err)
+	}
+
+	// Switch Log Level
+	switch viper.GetString("log_level") {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.SetLevel(log.WarnLevel)
+	}
 }
 
 func main() {
